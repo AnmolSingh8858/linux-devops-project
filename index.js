a4a5c53381b7bf4c5d86e162746c90176a5c18c8
Ubuntu <jenkins@Jenkins.0vi5devteohu3ahm54fistsuna.rx.internal.cloudapp.net>
2025-09-09 06:08:17 +0000

    Added index.js for jenkins CI/CD

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello from Node.js app!');
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
